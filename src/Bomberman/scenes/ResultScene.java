199265c2cfaf43afc588a3d46780fa5f671e976d
Darthveloper21 <hienvungoc01@gmail.com>
2020-11-05 13:17:55 +0700

    Win lose detector

---
+++ src/Bomberman/scenes/ResultScene.java
@@ -0,0 +1,33 @@
+package Bomberman.scenes;
+
+import Bomberman.Game;
+import Bomberman.graphics.MyButton;
+import Bomberman.graphics.gallery.Resources;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ResultScene extends MyScene {
+    private final MyButton back;
+    BufferedImage result;
+
+    public ResultScene(Game game) {
+        super(game);
+        back = new MyButton(game, Resources.back1, Resources.back2, 702, 640);
+    }
+
+    public void setResult(BufferedImage result) {
+        this.result = result;
+    }
+
+    @Override
+    public void update() {
+        back.update(game.getMenuScene());
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(result, 0, 0, null);
+        back.render(g);
+    }
+}
