6f7c43527800b5be8c3b2c4fa59d3b964a3c41d9
Darthveloper21 <hienvungoc01@gmail.com>
2020-11-21 14:23:33 +0700

    Different networks playable

--- src/Bomberman/network/Client.java
+++ src/Bomberman/network/Client.java
@@ -1,18 +1,34 @@
 package Bomberman.network;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    private static final String SERVER_IP = "127.0.0.1";
-    private static final int SERVER_PORT = 2521;
+    private static String SERVER_IP;
+    private static int SERVER_PORT;
 
     public static PrintWriter out;
     public static Socket socket;
+    public static boolean serverRunning = true;
+
+    private static void readPort() {
+        try {
+            File file = new File("resources/server.txt");
+            Scanner sc = new Scanner(file);
+            SERVER_IP = sc.nextLine();
+            SERVER_PORT = sc.nextInt();
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void runClient() {
         try {
+            readPort();
             socket = new Socket(SERVER_IP, SERVER_PORT);
 
             ServerConnection serverConn = new ServerConnection(socket);
@@ -20,6 +36,7 @@ public class Client {
 
             new Thread(serverConn).start();
         } catch (IOException e) {
+            serverRunning = false;
             e.printStackTrace();
         }
     }
--- src/Bomberman/network/ClientHandler.java
+++ src/Bomberman/network/ClientHandler.java
@@ -1,7 +1,5 @@
 package Bomberman.network;
 
-import Bomberman.Game;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
